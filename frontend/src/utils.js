71189498074b227d494f7fd54b05b3d132d4d60e
Rhythm1821 <rhythmrawat422@gmail.com>
2025-02-20 14:24:50 +0530

    utils file update

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -10,35 +10,6 @@ const fetchTemplates = async () => {
     return data;
 }
 
-const modalStyles = {
-    overlay: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-      background: '#fff',
-      padding: '20px',
-      borderRadius: '8px',
-      width: '400px',
-      textAlign: 'center',
-    },
-    closeButton: {
-      position: 'absolute',
-      top: '10px',
-      right: '10px',
-      border: 'none',
-      background: 'none',
-      fontSize: '16px',
-      cursor: 'pointer',
-    },
-  };
 
 const initialNodes = [
     // { id: 'initial', type: 'sequenceStartNode', position: { x: 500, y: 80 }, data: { label: 'Sequence start point' } },
@@ -47,4 +18,4 @@ const initialNodes = [
     { id: 'leadSource', type: 'leadSource', position: { x: 200, y: 80 }, data: { label: 'Add from Lead Source' } },
 ];
 
-export { fetchLeadSource, fetchTemplates, initialNodes,  modalStyles }  
+export { fetchLeadSource, fetchTemplates, initialNodes };
