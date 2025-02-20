3bb90e3e06e4794ff89d129a1b86da389b80aa19
Rhythm1821 <rhythmrawat422@gmail.com>
2025-02-20 14:24:15 +0530

    conditional to check if workflow is present

--- frontend/src/hooks/useFetchWorkflows.jsx
+++ frontend/src/hooks/useFetchWorkflows.jsx
@@ -4,7 +4,7 @@ export default function useFetchWorkflows(setNodes, setEdges) {
     useEffect(() => {
         fetch(`http://localhost:3000/api/workflows`)
           .then(res => res.json())
-          .then(data => data.allWorkflows[0])
+          .then(data => data.allWorkflows ? data.allWorkflows[0] : {})
           .then(data => {
             const { nodes, edges } = data;
             if (nodes && edges) {
