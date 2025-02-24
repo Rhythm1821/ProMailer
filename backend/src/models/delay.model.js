3a02a2ab4d8c0d8b88e2e20296cdf9198ba9dab8
Rhythm1821 <rhythmrawat422@gmail.com>
2025-02-24 15:50:43 +0530

    removed delay model

--- backend/src/models/delay.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const delaySchema = new Schema({
-    workflow: { type: Schema.Types.ObjectId, ref: 'Workflow' },
-    delayType:  { type: String, enum: ['Days', 'Hours', 'Minutes'], required: true },   
-    delayTime: { type: Number, required: true },
-}, { timestamps: true });
-
-export default mongoose.model('Delay', delaySchema);
