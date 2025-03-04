6a149d47f6634cff0022bd6eb9599871b6db69bb
Rhythm1821 <rhythmrawat422@gmail.com>
2025-03-04 23:12:19 +0530

    storing templates in array

--- backend/src/models/workflow.model.js
+++ backend/src/models/workflow.model.js
@@ -3,11 +3,11 @@ import mongoose, { Schema } from "mongoose";
 
 const workflowSchema = new Schema({
     lead: { type: Schema.Types.ObjectId, ref: 'Lead', required: true },
-    template: { type: Schema.Types.ObjectId, ref: 'Template', required: true },
-    delay: {
+    template: [{ type: Schema.Types.ObjectId, ref: 'Template', required: true }],
+    delay: [{
         time: Number,
         type: { type: String, enum: ['Days', 'Hours', 'Minutes'] }
-    },
+    }],
     nodes: { type: [Object], required: true },
     edges: { type: [Object], required: true },
 }, { timestamps: true });
